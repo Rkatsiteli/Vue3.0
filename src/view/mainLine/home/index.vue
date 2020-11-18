58de0ecdc6d6a4881da3b93344f60f6371913407
BaiYu(技术工程部_平台开发部) <baiyu@sogou-inc.com>
2020-11-18 21:53:40 +0800

    update

--- src/view/mainLine/home/index.vue
+++ src/view/mainLine/home/index.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>我是首页</div>
+  <div>我是首页{{ hello }}</div>
 </template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: {},
+  data() {
+    return {
+      hello: "Hello World",
+    };
+  },
+  setup(props) {
+    console.log(props);
+    console.log(this);
+    return {
+      props,
+    };
+  },
+  created() {
+    console.log("created");
+    console.log(this);
+  },
+  mounted() {
+    console.log("mounted");
+    console.log(this);
+  },
+  methods: {
+    showDrawer() {
+      this.visible = true;
+    },
+    onClose() {
+      this.visible = false;
+    },
+  },
+});
+</script>
